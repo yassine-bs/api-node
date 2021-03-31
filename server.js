9ec0864c150d8c9665eecf2bda21aa3ab6d35fba
yassine-bs <yassine@bearstudio.fr>
2021-03-31 14:09:01 +0100

    prod link update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+//require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to DB'))
