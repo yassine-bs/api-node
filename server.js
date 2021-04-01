6276184a817e2c49394da9e397f7213dc4ac938e
yassine-bs <yassine@bearstudio.fr>
2021-04-01 17:43:58 +0100

    add atlas-mongodb url

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const connexionUrl= 'mongodb+srv://user007:vHI7BVD6ck1rrON0@cluster0.m5sux.mongodb.net/users-db?retryWrites=true&w=majority';
+
+mongoose.connect(connexionUrl, { useNewUrlParser: true,useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to DB'))
